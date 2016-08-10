6f2bc7dea86b59089b75de66af1361481155d4cd
web corridor <webcorridor@webs-mac-mini.local>
2016-08-10 15:28:06 +0530

    Initial Commit

---
+++ jsonTestTests/jsonTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  jsonTestTests.swift
+//  jsonTestTests
+//
+//  Created by web corridor on 19/05/1938 Saka.
+//  Copyright © 1938 Saka web corridor. All rights reserved.
+//
+
+import XCTest
+@testable import jsonTest
+
+class jsonTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
